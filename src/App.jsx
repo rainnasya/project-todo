cd91025aaa5de6d839352456327475e79f7194b2
Rainnasya <rainnasyaaca@gmail.com>
2024-06-03 01:51:52 +0700

    Develop add todo feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Todos from './components/Todos'
+import TodoForm from './components/TodoForm'
 import './App.css'
 
 function App() {
@@ -34,9 +33,28 @@ function App() {
     setTodos(updatedTodos)
   }
 
+  const addTodo = (todoTitle) => {
+    if (todoTitle === '') {
+      return
+    }
+
+    const newTodo = {
+      id: todos.length + 1,
+      title: todoTitle,
+      completed: false,
+    }
+  
+    const updatedTodos = todos.concat(newTodo)
+    setTodos(updatedTodos)
+    
+  }
+
+  
+
   return (
     <div style={styles.container}>
       <h1 style={styles.title}>My Todo List</h1>
+      <TodoForm addTodo={addTodo}/>
       <Todos todos={todos} toggleCompleted={toggleCompleted} />
     </div>
   )
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -15,6 +15,7 @@ const TodoItem = ({todo, toggleCompleted}) => {
              style={styles.checkbox}
              onChange={() => toggleCompleted(todo.id)} />
             <p style={getTodoTitleStyle()}>{todo.title}</p>
+            <button style={styles.button}>X</button>
         </div>
     )
 }
@@ -24,13 +25,23 @@ const styles = {
         border: '2px solid #f4f4f4',
         fontSize: '24px',
         display: 'flex',
-        justifyContent: 'center',
+        justifyContent: 'space-between',
         alignItems: 'center',
+        padding: '0 20px',
       },
       checkbox: {
-        marginRight: '10px',
         height: '18px',
         width: '18px',
+      },
+      button: {
+        backgroundColor: '#BB0000',
+        color: '#fff',
+        height: '30px',
+        width: '30px',
+        borderRadius: '100%',
+        border: 'none',
+        cursor: 'pointer',
+        fontSize: '16px',
       },
 }
 
